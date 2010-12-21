4557ac861f9edd8d6ffb508d5143e8d9b87fd80e
Jasim A Basheer <jasim@c42.in>
2010-12-21 14:15:00 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WrestDemo::Application.config.secret_token = 'b265e7587f221c109370416063321b94917388b051aac1388d898740d21f050a65b16ab567593351bc7332e774763c1ab6390ac1575cf7ba1596da9614966944'
